2a1d9aa879398e0d4a798739db648f311369be8d
ericGTEC <128158333+ericDIT@users.noreply.github.com>
2025-07-31 13:47:14 +0200

    Initial commit, structure for project created as well as inclusion of OpenWeather API

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "Initialize Airflow..."
+airflow db init
+echo "Airflow db initialized"
+
+if airflow users list | awk '{print $1}' | grep -q "${AIRFLOW_ADMIN_USERNAME:-standard_admin}"; then 
+    echo "User '${AIRFLOW_ADMIN_USERNAME:-standard_admin}' already exists, skipping creation... "
+else 
+    echo "Creating new Admin-User... "
+    airflow users create \
+        --username "${AIRFLOW_ADMIN_USERNAME:-standard_admin}" \
+        --password "${AIRFLOW_ADMIN_PASSWORD:-standard_admin}" \
+        --firstname "${AIRFLOW_ADMIN_FIRSTNAME:-Admin}" \
+        --lastname "${AIRFLOW_ADMIN_LASTNAME:-User}" \
+        --role Admin \
+        --email "${AIRFLOW_ADMIN_EMAIL:-admin@test.com}"
+    echo "Admin-User created!"
+fi
+
+echo "Starting Airflow webserver"
+exec airflow webserver
+echo "Airflow webserver started"  
